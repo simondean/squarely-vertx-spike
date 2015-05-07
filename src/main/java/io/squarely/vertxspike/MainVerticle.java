6cfa5f5ddac4898718b3c0e5de7002c510c7137a
Simon Dean <simon@simondean.org>
2015-05-07 21:07:15 +0100

    Run Tiler as a verticle in this module instead of as a separate runnable module

--- src/main/java/io/squarely/vertxspike/MainVerticle.java
+++ src/main/java/io/squarely/vertxspike/MainVerticle.java
@@ -7,7 +7,7 @@ public class MainVerticle extends Verticle {
   public void start() {
     JsonObject config = container.config();
 
-    container.deployModule("io.tiler~tiler~0.1.2", config.getObject("server"), 1);
+    container.deployVerticle("io.tiler.ServerVerticle", config.getObject("server"), 1);
     container.deployVerticle("io.squarely.vertxspike.JenkinsCollectorVerticle", config.getObject("jenkins"), 1);
     container.deployVerticle("io.squarely.vertxspike.SonarQubeCollectorVerticle", config.getObject("sonarQube"), 1);
 
