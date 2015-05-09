7f6c68203c3d72bf2a040abc1ad8717a0f8692a3
Simon Dean <simon@simondean.org>
2015-05-09 15:59:52 +0100

    Moved Jenkins and SonarQube collectors to Tiler

--- src/main/java/io/squarely/vertxspike/CollectorVerticle.java
+++
@@ -1,6 +0,0 @@
-package io.squarely.vertxspike;
-
-import org.vertx.java.platform.Verticle;
-
-public class CollectorVerticle extends Verticle {
-}
--- src/main/java/io/squarely/vertxspike/MainVerticle.java
+++ src/main/java/io/squarely/vertxspike/MainVerticle.java
@@ -1,16 +1,34 @@
 package io.squarely.vertxspike;
 
+import org.vertx.java.core.Future;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
 
 public class MainVerticle extends Verticle {
-  public void start() {
+  public void start(final Future<Void> startedResult) {
     JsonObject config = container.config();
 
-    container.deployVerticle("io.tiler.ServerVerticle", config.getObject("server"), 1);
-    container.deployVerticle("io.squarely.vertxspike.JenkinsCollectorVerticle", config.getObject("jenkins"), 1);
-    container.deployVerticle("io.squarely.vertxspike.SonarQubeCollectorVerticle", config.getObject("sonarQube"), 1);
+    container.deployVerticle("io.tiler.ServerVerticle", config.getObject("server"), 1, result -> {
+      if (result.failed()) {
+        startedResult.setFailure(result.cause());
+        return;
+      }
 
-    container.logger().info("MainVerticle started");
+      container.deployModule("io.tiler~tiler-collector-jenkins~0.1.0", config.getObject("jenkins"), 1, result2 -> {
+        if (result.failed()) {
+          startedResult.setFailure(result.cause());
+          return;
+        }
+
+        container.deployModule("io.tiler~tiler-collector-sonarqube~0.1.0", config.getObject("sonarQube"), 1, result3 -> {
+          if (result.failed()) {
+            startedResult.setFailure(result.cause());
+            return;
+          }
+
+          container.logger().info("MainVerticle started");
+        });
+      });
+    });
   }
 }
