be90952404546d621e07c5a15e9ea103b8eeea2d
Simon Dean <simon@simondean.org>
2015-05-11 22:26:11 +0100

    Use vertx-async to simplify callbacks

--- src/main/java/io/squarely/vertxspike/MainVerticle.java
+++ src/main/java/io/squarely/vertxspike/MainVerticle.java
@@ -1,5 +1,6 @@
 package io.squarely.vertxspike;
 
+import org.simondean.vertx.async.Series;
 import org.vertx.java.core.Future;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
@@ -8,27 +9,18 @@ public class MainVerticle extends Verticle {
   public void start(final Future<Void> startedResult) {
     JsonObject config = container.config();
 
-    container.deployVerticle("io.tiler.ServerVerticle", config.getObject("server"), 1, result -> {
-      if (result.failed()) {
-        startedResult.setFailure(result.cause());
-        return;
-      }
-
-      container.deployModule("io.tiler~tiler-collector-jenkins~0.1.2", config.getObject("jenkins"), 1, result2 -> {
+    new Series<String>()
+      .task(handler -> container.deployVerticle("io.tiler.ServerVerticle", config.getObject("server"), 1, handler))
+      .task(handler -> container.deployModule("io.tiler~tiler-collector-jenkins~0.1.2", config.getObject("jenkins"), 1, handler))
+      .task(handler -> container.deployModule("io.tiler~tiler-collector-sonarqube~0.1.1", config.getObject("sonarQube"), 1, handler))
+      .run(result -> {
         if (result.failed()) {
           startedResult.setFailure(result.cause());
           return;
         }
 
-        container.deployModule("io.tiler~tiler-collector-sonarqube~0.1.1", config.getObject("sonarQube"), 1, result3 -> {
-          if (result.failed()) {
-            startedResult.setFailure(result.cause());
-            return;
-          }
-
-          container.logger().info("MainVerticle started");
-        });
+        container.logger().info("MainVerticle started");
+        startedResult.setResult(null);
       });
-    });
   }
 }
